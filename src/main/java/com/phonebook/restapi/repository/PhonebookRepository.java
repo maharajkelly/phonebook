6e3b34e8f0b0d9289262a4b207d2aa57e105c5d9
Kelly Maharaj <Kelly.Maharaj@standardbank.co.za>
2019-12-09 11:35:51 +0200

    Fullstack Development

--- src/main/java/com/phonebook/restapi/repository/PhonebookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.phonebook.restapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.phonebook.restapi.model.Phonebook;
-
-public interface PhonebookRepository extends JpaRepository<Phonebook, Long>{
-
-}
